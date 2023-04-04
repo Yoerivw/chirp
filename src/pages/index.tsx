4aba5510524db8c23902af5ba075682beafe7ff4
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2023-04-04 13:33:19 +0200

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { SignIn,SignInButton,SignOutButton, useUser } from "@clerk/nextjs";
-
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
   const user = useUser();
+
+  const {data} =  api.posts.getAll.useQuery();
 
   return (
     <>
@@ -22,6 +23,9 @@ const Home: NextPage = () => {
       <div>
   {!user.isSignedIn &&     <SignInButton /> }
    {!!user.isSignedIn && <SignOutButton />} 
+    </div>
+    <div>
+      {data?.map((post)=> (<div key={post.id}>{post.content}</div>))}
     </div>
       </main>
     </>
